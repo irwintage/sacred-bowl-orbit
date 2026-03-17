dab35931df1a8d5491c4269a11b461998d594c6f
irwintage <irwintage@gmail.com>
2026-03-17 18:39:57 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sacred-bowl-cache-v1";
+const CACHE_NAME = "sacred-bowl-cache-v2";
 
 const ASSETS = [
   "./",
@@ -13,7 +13,11 @@ const ASSETS = [
   "./fonts/inter-v20-latin-500.woff2"
 ];
 
+/* INSTALL */
+
 self.addEventListener("install", event => {
+
+  self.skipWaiting();
 
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
@@ -23,12 +27,44 @@ self.addEventListener("install", event => {
 
 });
 
+/* ACTIVATE */
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+
+});
+
+/* FETCH */
+
 self.addEventListener("fetch", event => {
 
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then(response => {
+
+        const clone = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, clone);
+        });
+
+        return response;
+
+      })
+      .catch(() => caches.match(event.request))
   );
 
 });
